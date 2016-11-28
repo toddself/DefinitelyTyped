70af3d02cbdb72fad5206f27b1d87125faa01cdc
Todd Kennedy <todd@selfassembled.org>
2016-11-28 10:52:10 -0800

    Add sheetify

---
+++ sheetify/sheetify-tests.ts
@@ -0,0 +1,14 @@
+import * as sheetify from 'sheetify';
+
+function done1 (err: Error, css: string, prefix: string) {};
+function done2 (err: Error, css: string) {};
+function done3 (err: Error) {};
+
+const test1 = sheetify('foobar');
+const test2 = sheetify('foobar', {global: true});
+const test3 = sheetify('foobar', 'beep.css', {global: true});
+const test4 = sheetify('foobar', 'beep.css');
+const test5 = sheetify('foobar', 'beep.css', {global: true}, done1);
+const test6 = sheetify('foobar', 'beep.css', {global: true}, done2);
+const test7 = sheetify('foobar', 'beep.css', {global: true}, done3);
+const prefix = sheetify.getPrefix('boop');
